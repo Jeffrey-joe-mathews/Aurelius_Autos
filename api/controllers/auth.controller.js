953eb91dab7e25956d54433a8a720aad1f68e530
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-03-30 18:05:19 +0530

    made the register endpoint

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,52 @@
-export const register = (req, res) => {
+import bcrypt from 'bcrypt'
+import prisma from '../lib/prisma.js';
+
+export const register = async (req, res) => {
     const {username, email, password} = req.body;
-    // hashing the password
+
+    // catching errors in user creation
+    try {
+        // hashing the password
+        const hashedPassword = await bcrypt.hash(password, 12);
+        // create a new user and save to the database
+        const newUser = await prisma.User.create({
+            data:{
+                username,
+                email,
+                password:hashedPassword,
+            }
+        })
+        res.status(201).json(
+            {
+                "success" : true,
+                "message" : "Account created sucessfully"
+            }
+        )
+        console.log(newUser)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json(
+            {
+                "success" : false,
+                "message" : "failed to create account"
+            }
+        )
+    }
 }
 export const login = (req, res) => {
     // db operations
+    const {username, password} = req.body;
+    try {
+
+        // check if the user exists
+        
+        // check if the password is correct
+        
+        // generate cookie token and send to the user
+    }
+    catch(error) {
+        console.error(error);
+    }
 }
 export const logout = (req, res) => {
     // db operations
