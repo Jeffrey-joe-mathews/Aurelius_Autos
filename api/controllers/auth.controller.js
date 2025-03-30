e9a787a4ac94e83d68b32e0730c617673c8acff3
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-03-30 18:30:31 +0530

    set up the login endpoint

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -33,19 +33,47 @@ export const register = async (req, res) => {
         )
     }
 }
-export const login = (req, res) => {
+export const login = async (req, res) => {
     // db operations
     const {username, password} = req.body;
     try {
 
         // check if the user exists
+        const user = await prisma.user.findUnique(
+            {
+                where:{username : username}
+            }
+        )
         
+        if(!user) {
+            res.status(401).json (
+                {
+                    "success" : false,
+                    "message" : "Account with the given username does not exist"
+                }
+            )
+        }
         // check if the password is correct
-        
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            res.status(401).json(
+                {
+                    "success" : true,
+                    "message" : "Invalid Credentials"
+                }
+            )
+        }
         // generate cookie token and send to the user
+        res.setHeader()
     }
     catch(error) {
         console.error(error);
+        res.status(500).json(
+            {
+                "success" : false,
+                "message" : "Server error probably ^w^"
+            }
+        )
     }
 }
 export const logout = (req, res) => {
