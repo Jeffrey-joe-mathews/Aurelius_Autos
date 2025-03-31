ecc49be3dd5571593cda972bb46cf052343be5c1
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-03-31 22:00:17 +0530

    making the cards and stuff

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom'
 import './Card.scss'
 
-const Card = () => {
+const Card = ({item}) => {
   return (
-    <div className='card'>Card</div>
+    <div className='card'>
+      <Link to={`/list/${item.id}`} className="imageContainer" >
+        <img src={item.img} alt={item.title} />
+      </Link>
+      <div className="txtContainer">
+        <h2 className="title">
+          <Link to={`/list/${item.id}`} >{item.title}</Link>
+        </h2>
+        <p className='address' >
+          <img src="/pin.png" alt="address" />
+          <span>{item.address}</span>
+        </p>
+        <p className='price' >$ {item.price}</p>
+      </div>
+    </div>
   )
 }
 
