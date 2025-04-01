e8159d080c9396f396f71bbe92feb4527bfdaf16
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-04-01 22:07:18 +0530

    making the image section in the item page

--- client/src/components/imgSlider/ImgSlider.jsx
+++ client/src/components/imgSlider/ImgSlider.jsx
@@ -1,8 +1,25 @@
 import './imgSlider.scss'
 
-const ImgSlider = () => {
+const ImgSlider = ({images}) => {
   return (
-    <div className='imgSlider'>ImgSlider</div>
+    <div className='imgSlider'>
+        <div className="full">
+            <div className="arr"><img src="/arrow.png" alt="previous" /></div>
+            <div className="imgContainer">
+                <img src={images[0]} alt="" />
+            </div>
+            <div className="arr"><img className='right' src="/arrow.png" alt="next" /></div>
+            <div className="closeBtn">(x)</div>
+        </div>
+        <div className="bigImage">
+            <img src={images[0]} alt='' />
+        </div>
+        <div className='smallImages'>
+            {images.slice(1).map((image, index) => (
+                <img src={image} alt="image" key={index} />
+            ))}
+        </div>
+    </div>
   )
 }
 
