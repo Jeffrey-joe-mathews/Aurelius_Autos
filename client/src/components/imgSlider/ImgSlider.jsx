d4f98e41a6b307a3d27e752ea4592aa3d06409d6
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-04-01 22:54:55 +0530

    made the image slider reponsive

--- client/src/components/imgSlider/ImgSlider.jsx
+++ client/src/components/imgSlider/ImgSlider.jsx
@@ -1,22 +1,49 @@
+import { useState } from 'react';
 import './imgSlider.scss'
 
 const ImgSlider = ({images}) => {
+
+    const [imageIndex, setImageIndex] = useState(null);
+
+    const changeSlides = (direction) => {
+        if (direction==="left") {
+            if( imageIndex === 0 ) {
+                setImageIndex(images.length-1);
+            }
+            else {
+                setImageIndex(imageIndex-1)
+            }
+        }
+            else {
+            if( imageIndex === images.length-1 ) {
+                setImageIndex(0);
+            }
+            else {
+                setImageIndex(imageIndex+1)
+            }
+        } 
+    }
+
   return (
-    <div className='imgSlider'>
+      <div className='imgSlider'>
+        { imageIndex !== null && (
         <div className="full">
-            <div className="arr"><img src="/arrow.png" alt="previous" /></div>
+            <div className="arr"><img src="/arrow.png" alt="previous" onClick={() => changeSlides("left")} /></div>
             <div className="imgContainer">
-                <img src={images[0]} alt="" />
+                <img src={images[imageIndex]} alt="" />
             </div>
-            <div className="arr"><img className='right' src="/arrow.png" alt="next" /></div>
-            <div className="closeBtn">(x)</div>
+            <div className="arr"><img className='right' src="/arrow.png" alt="next" onClick={() => changeSlides("right")} /></div>
+            <div className="closeBtn" onClick={() => setImageIndex(null)} >(x)</div>
         </div>
+        )}
         <div className="bigImage">
-            <img src={images[0]} alt='' />
+            <img src={images[0]} alt='' onClick={() => setImageIndex(
+                () => (0)
+            )} />
         </div>
         <div className='smallImages'>
             {images.slice(1).map((image, index) => (
-                <img src={image} alt="image" key={index} />
+                <img src={image} alt="image" key={index} onClick={() => setImageIndex(index+1)} />
             ))}
         </div>
     </div>
