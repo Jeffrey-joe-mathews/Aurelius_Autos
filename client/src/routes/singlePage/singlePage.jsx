73a3b7c9950cb86ca94023cadd55ba7a0143b65d
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-04-02 07:05:03 +0530

    completed the item page

--- client/src/routes/singlePage/singlePage.jsx
+++ client/src/routes/singlePage/singlePage.jsx
@@ -1,6 +1,7 @@
 import ImgSlider from '../../components/imgSlider/ImgSlider'
 import './singlePage.scss'
 import { singlePostData, userData } from '../../lib/dummyData.js'
+import Map from '../../components/map/Map.jsx'
 
 const SinglePage = () => {
   return (
@@ -29,7 +30,61 @@ const SinglePage = () => {
       </div>
       <div className="features">
         <div className="wrapper">
-          
+          <p className='title' >General</p> 
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>renter is responsible</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>renter is responsible</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>renter is responsible</p>
+              </div>
+            </div>
+          </div>
+          <p className='title' >More Details</p> 
+          <div className="sizes">
+            <div className="size">
+              <img src="/size.png" alt="" />
+              <span>sometext</span>
+            </div>
+            <div className="size">
+              <img src="/size.png" alt="" />
+              <span>sometext</span>
+            </div>
+            <div className="size">
+              <img src="/size.png" alt="" />
+              <span>sometext</span>
+            </div>
+          </div>
+          <p className='title' >Some Shit</p> 
+          <div className="listHorizontal"></div>
+          <p className='title' >some more shit</p>
+          <div className="mapContainer">
+            <Map items ={[singlePostData]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="" />
+              send a message
+            </button>
+            <button>
+              <img src="/save.png" alt="" />
+              save the dealer
+            </button>
+          </div>
         </div>
       </div>
     </div>
