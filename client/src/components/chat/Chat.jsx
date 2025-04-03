718416744b85abf2c88a98f3532df07ee0ebbc3f
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-04-03 06:48:45 +0530

    added the chatbox

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import './chat.scss'
 
 const Chat = () => {
+
+    const [chat, setChat] = useState(true);
+
   return (
     <div className='chat'>
         <div className="messages">
@@ -55,9 +59,40 @@ const Chat = () => {
                 </p>
             </div>
         </div>
+
+    {chat && 
         <div className="chatBox">
-            <div className="top"></div>
+            <div className="top">
+                <div className="user">
+                    <img src="https://avatars.githubusercontent.com/u/171322141?v=4" alt="" />
+                    Jeffrey Joe
+                </div>
+                <span className='close' onClick={() => setChat(() => !chat)} >x</span>
+            </div>
+            <div className="center">
+                <div className="chatMessage">
+                    <p>Lorem ipsum dolor sit.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage own">
+                    <p>Lorem ipsum dolor sit.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage own">
+                    <p>Lorem ipsum dolor sit.</p>
+                    <span>1 hour ago</span>
+                </div>
+                <div className="chatMessage">
+                    <p>Lorem ipsum dolor sit.</p>
+                    <span>1 hour ago</span>
+                </div>
+            </div>
+            <div className="bottom">
+                <textarea></textarea>
+                <button>Send</button>
+            </div>
         </div>
+        }
     </div>
   )
 }
