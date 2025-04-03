ef66f6d4c28162cddf9aa90e37e33538950be086
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-04-03 05:39:34 +0530

    made changes to mesage section

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -40,8 +40,24 @@ const Chat = () => {
                     Lorem ipsum dolor sit.
                 </p>
             </div>
+            <div className='message' >
+                <img src="https://avatars.githubusercontent.com/u/171322141?v=4" alt="" />
+                <span>Jeffrey Joe</span>
+                <p>
+                    Lorem ipsum dolor sit.
+                </p>
+            </div>
+            <div className='message' >
+                <img src="https://avatars.githubusercontent.com/u/171322141?v=4" alt="" />
+                <span>Jeffrey Joe</span>
+                <p>
+                    Lorem ipsum dolor sit.
+                </p>
+            </div>
         </div>
-        <div className="chatBox">box</div>
+        <div className="chatBox">
+            <div className="top"></div>
+        </div>
     </div>
   )
 }
